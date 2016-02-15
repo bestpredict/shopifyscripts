89faf0a87c0fbab9c0ed01496329dfc147ee0f5b
Yitzchok Gottlieb <adminjew@gmail.com>
2016-02-15 16:51:25 -0500

    cleanup code

--- bestpredict.js
+++ bestpredict.js
@@ -212,12 +212,14 @@
                 return api.MoneyFormat.replace("{{amount}}", val);
             },
             formatMoney: function (n, decPlaces, thouSeparator, decSeparator) {
-                var decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
-                    decSeparator = decSeparator == undefined ? "." : decSeparator,
-                    thouSeparator = thouSeparator == undefined ? "," : thouSeparator,
-                    sign = n < 0 ? "-" : "",
-                    i = parseInt(n = Math.abs(+n || 0).toFixed(decPlaces)) + "",
-                    j = (j = i.length) > 3 ? j % 3 : 0;
+                decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces;
+                decSeparator = decSeparator == undefined ? "." : decSeparator;
+                thouSeparator = thouSeparator == undefined ? "," : thouSeparator;
+
+                var sign = n < 0 ? "-" : "",
+                  i = parseInt(n = Math.abs(+n || 0).toFixed(decPlaces)) + "",
+                  j = (j = i.length) > 3 ? j % 3 : 0;
+
                 return sign + (j ? i.substr(0, j) + thouSeparator : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thouSeparator) + (decPlaces ? decSeparator + Math.abs(n - i).toFixed(decPlaces).slice(2) : "");
             }
         }
